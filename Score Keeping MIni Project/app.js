50350b0cce7db4989bb7feca68d04d73a4bfdbd4
TyErdos <44851615+TyErdos@users.noreply.github.com>
2023-01-19 16:53:17 -0500

    Score keeping website cerated using bulma css, JS & HTML

---
+++ Score Keeping MIni Project/app.js
@@ -0,0 +1,78 @@
+const p1 =
+{
+    score: 0,
+    button: document.querySelector("#p1Button"),
+    display: document.querySelector('#p1ScoreDisplay')
+
+}
+
+const p2 =
+{
+    score: 0,
+    button: document.querySelector("#p2Button"),
+    display: document.querySelector('#p2ScoreDisplay')
+
+}
+
+const resetButton = document.querySelector('#reset');
+
+const winningScoreSelect = document.querySelector('#playTo');
+
+let winningScore = 5;
+
+let isGameOver = false;
+
+
+function updateScores(player, opponent)
+{
+    if(!isGameOver)
+    {
+        player.score++;
+
+        if(player.score === winningScore)
+            {
+                isGameOver = true;
+                player.display.classList.add('has-text-success');
+                opponent.display.classList.add('has-text-danger');
+                player.button.disabled=true;
+                opponent.button.disabled=true;
+                
+            }
+            player.display.textContent = player.score;
+    }
+
+
+}
+
+p1.button.addEventListener('click', function(e)
+{
+    updateScores(p1,p2);
+});
+
+
+p2.button.addEventListener('click', function(e)
+{
+    updateScores(p2,p1);
+});
+
+function reset()
+{
+    isGameOver = false;
+
+    for(let p of [p1,p2])
+    {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success','has-text-danger');
+        p.button.disabled = false;
+    }
+};
+
+resetButton.addEventListener('click', reset);
+
+winningScoreSelect.addEventListener('change', function(e)
+{
+    winningScore = parseInt(this.value);
+    reset();
+
+});
